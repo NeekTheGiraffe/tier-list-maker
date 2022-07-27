395c497f6a6529903f51dc4ebf0681a9b84c45e4
Nick Pavlosky <44681231+NeekTheGiraffe@users.noreply.github.com>
2022-07-27 17:39:25 -0400

    Add Upload button; add tooltips

--- sketch.js
+++ sketch.js
@@ -16,8 +16,8 @@ function setup() {
     tierList = new TierList(TIER_MARGIN, TIER_MARGIN, width - 2*TIER_MARGIN, height - 2*TIER_MARGIN);
     mouseHandler = new MouseHandler(tierList);
 
-    const images = ['elephant.png', 'snake.png', 'walrus.png', 'zebra.png'];
-    images.forEach(path => loadImage(`assets/${path}`, img => tierList.addItemWithImage(img)));
+    //const images = ['elephant.png', 'snake.png', 'walrus.png', 'zebra.png'];
+    //images.forEach(path => loadImage(`assets/${path}`, img => tierList.addItemWithImage(img)));
 }
 
 function draw() {
@@ -43,10 +43,14 @@ function mouseReleased() {
     mouseHandler.handleMouseReleased(mouseX, mouseY);
 }
 
+// --- Button event listeners --- 
+
 function onAddImageButtonClick(e) {
     const input = document.getElementById('image-input');
-    //console.log(input.value);
-    loadImage(input.value, img => tierList.addItemWithImage(img));
+    const urls = input.value.split(',');
+    urls.forEach(url => {
+        loadImage(url.trim(), img => tierList.addItemWithImage(img));
+    });
     input.value = '';
 }
 
@@ -54,5 +58,20 @@ function onDownloadButtonClick(e) {
     saveCanvas('tier-list');
 }
 
+function onUploadFiles(e) {
+    const fileInput = document.getElementById('file-input');
+    const files = fileInput.files;
+    //console.log(files);
+
+    files.forEach(file => {
+        const objectURL = window.URL.createObjectURL(file);
+        //console.log(objectURL);
+        loadImage(objectURL, img => tierList.addItemWithImage(img));
+    });
+
+    fileInput.value = null;
+}
+
 document.getElementById('download-button').addEventListener('click', onDownloadButtonClick);
 document.getElementById('add-image-button').addEventListener('click', onAddImageButtonClick);
+document.getElementById('file-input').addEventListener('change', onUploadFiles);
